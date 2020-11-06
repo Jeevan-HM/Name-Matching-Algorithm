51161785128f7ed88110371738ff5938c4348c10
Jeevan HM <jeevanhm308@gmail.com>
2020-11-06 21:02:38 +0530

    Improve accuracy of name matching algorithm

---
+++ api.py
@@ -0,0 +1,144 @@
+from unicodedata import normalize
+from flask import Flask, request, jsonify, Response
+from flask.json import htmlsafe_dump
+from flask_restful import Api, Resource, reqparse, abort
+import enchant
+import Levenshtein
+import re
+import unicodedata
+import jellyfish
+import json
+
+app = Flask(__name__)
+api = Api(app)
+
+
+names_put_args = reqparse.RequestParser()
+names_put_args.add_argument("raw_input_name1", type=str, required=True)
+names_put_args.add_argument("raw_input_name2", type=str, required=True)
+
+# ? Clean the name by removing all special characters and other unicode symbols
+def filter_unicode_characters(data):
+    prefixes = [
+        "MR.",
+        "MRS.",
+        "MS.",
+        "MR",
+        "MRS",
+        "MS",
+        "DR",
+        "DR.",
+        "SH",
+        "SH.",
+        "S/O",
+        "W/O",
+        "D/O",
+        "SON OF",
+        "MR.",
+        "MRS.",
+        "MS.",
+        "MS",
+        "MRS",
+        "MS",
+        "SHRI",
+        "SHREE",
+        "SHRE",
+        "SRI",
+        "DR",
+        "DR.",
+        "SH",
+        "SH.",
+    ]
+    for i in range(len(prefixes)):
+        prefixes[i] = prefixes[i].lower()
+    name_in_ascii = unicodedata.normalize("NFKD", data).encode("ASCII", "ignore")
+    filtered_name = name_in_ascii.decode("utf-8")
+    filtered_name = filtered_name.lower()
+    regex = r"\b(?:" + "|".join(prefixes) + r")\.\s*"
+    filtered_name = re.sub(regex, "", filtered_name)
+    filtered_name = re.sub("[^A-Za-z]+", "", filtered_name)
+    filtered_name = re.sub(" +", "", filtered_name)
+    return filtered_name
+
+
+# ? Sort the first name and last name alphabetically
+def sort_words(input_name):
+
+    input_name = input_name.split(" ")
+    input_name.sort()
+    sorted_name = " ".join(input_name)
+    return sorted_name
+
+
+# ? Find the soundex value for the cleaned word
+def soundex(filtered_name: str):
+    soundex_representation = jellyfish.soundex(filtered_name)
+    return soundex_representation
+
+
+class name_matching(Resource):
+    def get(self, raw_input_name1, raw_input_name2):
+        try:
+            raw_input_name1 = sort_words(raw_input_name1)
+            raw_input_name2 = sort_words(raw_input_name2)
+            filtered_name_1 = filter_unicode_characters(raw_input_name1)
+            filtered_name_2 = filter_unicode_characters(raw_input_name2)
+            soundex_representation_name1 = soundex(filtered_name_1)
+            soundex_representation_name2 = soundex(filtered_name_2)
+            levenshtein_distance = enchant.utils.levenshtein(
+                filtered_name_1.lower(), filtered_name_2.lower()
+            )
+            levenshtein_ratio = round(
+                (Levenshtein.ratio(filtered_name_1, filtered_name_2) * 100)
+            )
+            if len(filtered_name_1) >= 3 and len(filtered_name_2) >= 3:
+                if levenshtein_distance <= 1 or levenshtein_ratio >= 85:
+                    status = True
+                    message = (
+                        "Levenshtein ratio["
+                        + str(levenshtein_ratio)
+                        + "],The names match",
+                    )
+
+                elif soundex_representation_name1 == soundex_representation_name2:
+                    status = True
+                    message = "Soundex representation is the same, the names match"
+                else:
+                    status = False
+                    message = "The names do not match"
+
+            else:
+                status = False
+                message = "Name length too small, name must be at least 3 characters"
+                return jsonify({"status": status, "message": message})
+            json_response = {
+                "raw_input": {"name_1": raw_input_name1, "name_2": raw_input_name2},
+                "filtered_input": {
+                    "name_1": filtered_name_1,
+                    "name_2": filtered_name_2,
+                },
+                "levenshtein_ratio": levenshtein_ratio,
+                "levenshtein_distance": levenshtein_distance,
+                "soundex_representaion": {
+                    "name_1": soundex_representation_name1,
+                    "name_2": soundex_representation_name2,
+                },
+                "status": status,
+                "message": message,
+            }
+            return jsonify(json_response)
+
+        except Exception as e:
+            print(e)
+            return Response(
+                response=json.dumps({e}, default=str),
+                status=404,
+                mimetype="application/json",
+            )
+
+
+api.add_resource(
+    name_matching, "/name/<string:raw_input_name1>/<string:raw_input_name2>"
+)
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ test.py
@@ -0,0 +1,292 @@
+try:
+    from api import app
+    import unittest
+    import pprint
+    import json
+    from dictor import dictor
+    from mock import patch
+    import requests
+except Exception as e:
+    print("Some Modules are Missing".format(e))
+
+try:
+    global pass_test_name_1, pass_test_name_2, fail_test_name_1, fail_test_name_2, empty_input_1, empty_input_2
+    pass_test_name_1 = "John"
+    pass_test_name_2 = "J1Ohn"
+    fail_test_name_1 = "John"
+    fail_test_name_2 = "Jacob"
+    empty_input_1 = ""
+    empty_input_2 = ""
+
+    class url_funcionality_check(unittest.TestCase):
+        # ? Check for response 200
+        def test_status_code_get(self):
+            tester = app.test_client(self)
+            response = tester.get("/name/%s/%s")
+            statuscode = response.status_code
+            print("[Success]Status " + str(statuscode))
+            self.assertEqual(statuscode, 200)
+
+        # ? Response for POST requests
+        def test_status_code_post(self):
+            tester = app.test_client(self)
+            response = tester.post("/name/%s/%s")
+            statuscode = response.status_code
+            print("[Success]POST method not allowed-" + str(statuscode))
+            self.assertEqual(statuscode, 405)
+
+        # ? Response for DELETE requests
+        def test_status_code_post_delete(self):
+            tester = app.test_client(self)
+            response = tester.delete("/name/%s/%s")
+            statuscode = response.status_code
+            print("[Success]DELETE method not allowed-" + str(statuscode))
+            self.assertEqual(statuscode, 405)
+
+        # ? Check if response type is JSON
+        def test_response_type_is_json(self):
+            tester = app.test_client(self)
+            response = tester.get("/name/%s/%s")
+            content_type = response.content_type
+            print("[Success]Content Type ", content_type)
+            self.assertEqual(response.content_type, "application/json")
+
+        # ? Check if the URL exists
+        def test_returns_false_if_url_doesnt_exist(self):
+            tester = app.test_client(self)
+            url = "/"
+            response = tester.get(url)
+            statuscode = response.status_code
+            print("[Success]URL '/' doesn't exist!")
+            self.assertEqual(statuscode, 404)
+
+        # ? Check if URL has less than the required params
+        def test_returns_false_if_url_not_found(self):
+            tester = app.test_client(self)
+            url = "/name/"
+            response = tester.get(url)
+            statuscode = response.status_code
+            print("[Success]URL has less/no params!")
+            self.assertEqual(statuscode, 404)
+
+        # ? Check if URL has more than one params
+        def test_returns_false_if_url_has_many_inputs(self):
+            tester = app.test_client(self)
+            url = "/name/" + fail_test_name_1
+            response = tester.get(
+                "/name/" + fail_test_name_1 + "/" + fail_test_name_2 + "/"
+            )
+            statuscode = response.status_code
+            print("[Success]URL has too many params!")
+            self.assertEqual(statuscode, 404)
+
+    # ? Check test cases for levenshtein distance
+    class test_levenshtein_diatance(unittest.TestCase):
+
+        # ? Check if levenshtein distance works!
+        def test_levenshtein_distance_success(self):
+            tester = app.test_client(self)
+            response = tester.get("/name/" + pass_test_name_1 + "/" + pass_test_name_2)
+            data = json.loads(response.get_data(as_text=True))
+            levenshtein_distance = data["levenshtein_distance"]
+            assertion_levenshtein_distance = 0
+            if levenshtein_distance <= 2:
+                assertion_levenshtein_distance = 1
+            print(
+                "[Success]Names match, levenshtein distance [%d] between '%s' and '%s'"
+                % (levenshtein_distance, pass_test_name_1, pass_test_name_2)
+            )
+            self.assertEqual(assertion_levenshtein_distance, 1)
+
+        # ? Check if levenshtein distance fails test case
+        def test_levenshtein_distance_failure(self):
+            tester = app.test_client(self)
+            response = tester.get("/name/" + fail_test_name_1 + "/" + fail_test_name_2)
+            data = json.loads(response.get_data(as_text=True))
+            levenshtein_distance = data["levenshtein_distance"]
+            assertion_levenshtein_distance = 1
+            if levenshtein_distance > 2:
+                assertion_levenshtein_distance = 0
+            print(
+                "[Success]Names don't match, levenshtein distance [%d] between '%s' and '%s'"
+                % (levenshtein_distance, fail_test_name_1, fail_test_name_2)
+            )
+
+            self.assertEqual(assertion_levenshtein_distance, 0)
+
+        # ? Check if the levenshtein params are empty
+        def test_levenshtein_distancefailure_no_params(self):
+            tester = app.test_client(self)
+            response = tester.get("/name/" + empty_input_1 + "/" + empty_input_2)
+            assertion_levenshtein_distance = 1
+            if empty_input_1 == "" or empty_input_2 == "":
+                assertion_levenshtein_distance = 0
+            print("[Success]Both Params are empty for levenshtein distance")
+            self.assertEqual(assertion_levenshtein_distance, 0)
+
+        # ? Check if the levenshtein first param is empty
+        def test_levenshtein_distance_failure_empty_first_param(self):
+            tester = app.test_client(self)
+            response = tester.get("/name/" + empty_input_1 + "/" + pass_test_name_1)
+            assertioin_soundex_represent = 1
+            if empty_input_1 == "" or pass_test_name_1 == "":
+                assertioin_soundex_represent = 0
+            print("[Success]First Param is empty for levenshtein distance")
+            self.assertEqual(assertioin_soundex_represent, 0)
+
+        # ? Check if the levenshtein second param is empty
+        def test_levenshtein_distance_failure_empty_second_param(self):
+            tester = app.test_client(self)
+            response = tester.get("/name/" + pass_test_name_2 + "/" + empty_input_2)
+            assertioin_soundex_represent = 1
+            if pass_test_name_2 == "" or empty_input_2 == "":
+                assertioin_soundex_represent = 0
+            print("[Success]Second Param is empty for levenshtein distance")
+            self.assertEqual(assertioin_soundex_represent, 0)
+
+    # ? Check test cases for soundex
+    class test_soundex_representation(unittest.TestCase):
+        # ? Test if Soundex passes the test case
+        def test_soundex_representation_success(self):
+            tester = app.test_client(self)
+            response = tester.get("/name/" + pass_test_name_1 + "/" + pass_test_name_2)
+            data = json.loads(response.get_data(as_text=True))
+            soundex_name_1 = dictor(data, "soundex_representaion.name_1")
+            soundex_name_2 = dictor(data, "soundex_representaion.name_2")
+            assertioin_soundex_represent = 0
+            if soundex_name_1 == soundex_name_2:
+                assertioin_soundex_represent = 1
+
+            print(
+                "[Success]Names match, Soundex representation is '%s'[%s] and '%s'[%s]"
+                % (
+                    soundex_name_1,
+                    pass_test_name_1,
+                    soundex_name_2,
+                    pass_test_name_2,
+                )
+            )
+
+            self.assertEqual(assertioin_soundex_represent, 1)
+
+        # ? Check if Soundex fails the test case
+        def test_soundex_representation_failure(self):
+            tester = app.test_client(self)
+            response = tester.get("/name/" + fail_test_name_1 + "/" + fail_test_name_2)
+            data = json.loads(response.get_data(as_text=True))
+            soundex_name_1 = dictor(data, "soundex_representaion.name_1")
+            soundex_name_2 = dictor(data, "soundex_representaion.name_2")
+            assertioin_soundex_represent = 1
+            if soundex_name_1 != soundex_name_2:
+                assertioin_soundex_represent = 0
+            print(
+                "[Success]Names don't match, Soundex representation is '%s'[%s] and '%s'[%s]"
+                % (
+                    soundex_name_1,
+                    fail_test_name_1,
+                    soundex_name_2,
+                    fail_test_name_2,
+                )
+            )
+            self.assertEqual(assertioin_soundex_represent, 0)
+
+        # ? Check is soundex params are empty!
+        def test_soundex_representation_failure_empty_params(self):
+            tester = app.test_client(self)
+            response = tester.get("/name/" + empty_input_1 + "/" + empty_input_2)
+            assertioin_soundex_represent = 1
+            if empty_input_1 == "" or empty_input_2 == "":
+                assertioin_soundex_represent = 0
+            print("[Success]Both Params are empty for soundex representation")
+            self.assertEqual(assertioin_soundex_represent, 0)
+
+        # ? Check if first soundex param is empty
+        def test_soundex_representation_failure_empty_first_param(self):
+            tester = app.test_client(self)
+            response = tester.get("/name/" + empty_input_1 + "/" + pass_test_name_1)
+            assertioin_soundex_represent = 1
+            if empty_input_1 == "":
+                assertioin_soundex_represent = 0
+            print("[Success]First Param is empty for soundex representation")
+            self.assertEqual(assertioin_soundex_represent, 0)
+
+        # ? Check is second soundex param is empty
+        def test_soundex_representation_failure_empty_second_param(self):
+            tester = app.test_client(self)
+            response = tester.get("/name/" + pass_test_name_2 + "/" + empty_input_2)
+            assertioin_soundex_represent = 1
+            if empty_input_2 == "":
+                assertioin_soundex_represent = 0
+            print("[Success]Second Param is empty for soundex representation")
+            self.assertEqual(assertioin_soundex_represent, 0)
+
+    class test_levenshtein_ratio(unittest.TestCase):
+
+        # ? Check if levenshtein ratio works!
+        def test_levenshtein_ratio_success(self):
+            tester = app.test_client(self)
+            response = tester.get("/name/" + pass_test_name_1 + "/" + pass_test_name_2)
+            data = json.loads(response.get_data(as_text=True))
+            levenshtein_ratio = data["levenshtein_ratio"]
+            assertion_levenshtein_ratio = 0
+            if levenshtein_ratio >= 85:
+                assertion_levenshtein_ratio = 1
+            print(
+                "[Success]Names match, levenshtein ratio [%d] between '%s' and '%s'"
+                % (levenshtein_ratio, pass_test_name_1, pass_test_name_2)
+            )
+            self.assertEqual(assertion_levenshtein_ratio, 1)
+
+        # ? Check if levenshtein distance fails test case
+        def test_levenshtein_ratio_failure(self):
+            tester = app.test_client(self)
+            response = tester.get("/name/" + fail_test_name_1 + "/" + fail_test_name_2)
+            data = json.loads(response.get_data(as_text=True))
+            levenshtein_ratio = data["levenshtein_ratio"]
+            assertion_levenshtein_ratio = 1
+            if levenshtein_ratio < 85:
+                assertion_levenshtein_ratio = 0
+            print(
+                "[Success]Names don't match, levenshtein ratio [%d] between '%s' and '%s'"
+                % (levenshtein_ratio, fail_test_name_1, fail_test_name_2)
+            )
+
+            self.assertEqual(assertion_levenshtein_ratio, 0)
+
+        # ? Check if the levenshtein params are empty
+        def test_levenshtein_ratio_failure_no_params(self):
+            tester = app.test_client(self)
+            response = tester.get("/name/" + empty_input_1 + "/" + empty_input_2)
+            assertion_levenshtein_ratio = 1
+            if empty_input_1 == "" or empty_input_2 == "":
+                assertion_levenshtein_ratio = 0
+            print("[Success]Both Params are empty for levenshtein ratio")
+            self.assertEqual(assertion_levenshtein_ratio, 0)
+
+        # ? Check if the levenshtein first param is empty
+        def test_levenshtein_ratio_failure_empty_first_param(self):
+            tester = app.test_client(self)
+            response = tester.get("/name/" + empty_input_1 + "/" + pass_test_name_1)
+            assertion_levenshtein_ratio = 1
+            if empty_input_1 == "" or pass_test_name_1 == "":
+                assertion_levenshtein_ratio = 0
+            print("[Success]First Param is empty for levenshtein ratio")
+            self.assertEqual(assertion_levenshtein_ratio, 0)
+
+        # ? Check if the levenshtein second param is empty
+        def test_levenshtein_distance_failure_empty_second_param(self):
+            tester = app.test_client(self)
+            response = tester.get("/name/" + pass_test_name_2 + "/" + empty_input_2)
+            assertion_levenshtein_ratio = 1
+            if pass_test_name_2 == "" or empty_input_2 == "":
+                assertion_levenshtein_ratio = 0
+            print("[Success]Second Param is empty for levenshtein distance")
+            self.assertEqual(assertion_levenshtein_ratio, 0)
+
+
+except Exception as e:
+    print("Something went wrong!!".format(e))
+
+
+if __name__ == "__main__":
+    unittest.main()
